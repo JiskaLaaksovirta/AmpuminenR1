574314b162ad62175d610f1b3827a20200c173b9
Jiska <jiska96@gmail.com>
2024-02-03 15:07:48 +0200

    Pienmuutoksia Jonin luennon mukaisesti

--- AmpuminenR1/Program.cs
+++ AmpuminenR1/Program.cs
@@ -4,3 +4,5 @@ using System.Runtime.CompilerServices;
 Console.WriteLine("Moikka maailma, tämä on Jiskan kirjoitus");
 Console.WriteLine("Tervehdys kaikille tämä on Inkan testi kirjoitus");
 Console.WriteLine("Terve! Eveliina testaa.");
+
+Console.WriteLine("Tämä on testirivi - Jiska");
