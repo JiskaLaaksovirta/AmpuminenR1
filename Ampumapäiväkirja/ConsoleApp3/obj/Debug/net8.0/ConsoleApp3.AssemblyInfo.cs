e8a030f7412af282f4881c3b40da44ff7350d261
InkaKaalikoski <you@example.com>
2024-02-16 16:58:30 +0200

    Retro ja 2. sprintin aloitus palaveri -IK

--- Ampumapäiväkirja/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ Ampumapäiväkirja/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dbf473d2e963c8b8ddce8619cbae90a0de97e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
