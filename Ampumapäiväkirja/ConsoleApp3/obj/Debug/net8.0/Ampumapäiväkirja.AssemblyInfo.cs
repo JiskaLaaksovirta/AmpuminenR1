a26191f84dcea41e76c846a3f30cd23d3ad79262
EveTuomioja <essi23013@student.hamk.fi>
2024-02-25 22:20:01 +0200

    UML ja ajankäytön päivitys

--- Ampumapäiväkirja/ConsoleApp3/obj/Debug/net8.0/Ampumapäiväkirja.AssemblyInfo.cs
+++ Ampumapäiväkirja/ConsoleApp3/obj/Debug/net8.0/Ampumapäiväkirja.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ampumapäiväkirja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1495c6808db59228e1a13f8229c7a962098fb6e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66cdd7c6835e686675776b2ec8d2c6a1dc4d448")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ampumapäiväkirja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ampumapäiväkirja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
